90b65484774472df9574a1496e61c851d96a1ba8
jingug1004 <jingug1004@gmail.com>
2021-05-29 15:13:09 +0900

    210529 (토) 기본적인 수치 계산 - 반올림과 절댓값 구하기

---
+++ a21/a03.py
@@ -0,0 +1,14 @@
+import math
+
+a01 = math.ceil(3.14)
+print(a01)
+a01 = math.floor(3.14)
+print(a01)
+a01 = math.floor(-3.14)
+print(a01)
+a01 = math.trunc(3.14)
+print(a01)
+a01 = math.trunc(-3.14)
+print(a01)
+a01 = math.fabs(-3.14)
+print(a01)

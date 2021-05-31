378c0333381d09fe8e7fceac52bf5e7bcfa61445
jingug1004 <jingug1004@gmail.com>
2021-05-31 21:29:45 +0900

    210531 (월) 기본적인 수치 계산 - 원주율과 자연로그의 값 구하기

---
+++ a21/a04.py
@@ -0,0 +1,21 @@
+import math
+
+a01 = math.pi
+print(a01)
+a01 = math.e
+print(a01)
+a01 = math.log(1.1, 10)
+print(a01)
+a01 = math.log10(1.1)
+print(a01)
+
+a01 = math.pow(2, 3)
+print(a01)
+a01 = pow(2, 3)
+print(a01)
+a01 = 2**3
+print(a01)
+a01 = pow(2, 3.0)
+print(a01)
+a01 = pow(2.0, 3)
+print(a01)

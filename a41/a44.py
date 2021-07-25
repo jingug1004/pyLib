e2430c4e46bf39b2ada3e1a96b16a5deb8acb3f8
jingug1004 <jingug1004@gmail.com>
2021-07-25 16:05:00 +0900

    210725 (일) 자료형과 알고리즘 - 다양한 컨테너이너형 다루기 - 튜플을 구조체로 활용하기

--- a41/a44.py
+++ a41/a44.py
@@ -1,15 +1,6 @@
 import collections
 
-d = collections.OrderedDict()
-d['spam'] = 100
-d['ham'] = 200
-
-for key in d:
-    print(key)
-
-e = collections.OrderedDict([('spam', 100), ('ham', 200)])
-print(e)
-f = collections.OrderedDict({'spam': 100, 'ham': 200})
-print(f)
-g = collections.OrderedDict(spam=100, ham=200)
-print(g)
+Coordinate = collections.namedtuple('Coordinate1', 'X, Y, Z')
+c1 = Coordinate(100, -50, 200)
+print(c1)
+print(c1.X)
